c1b08265c5cad1dfa68785fa514b56221f067b1a
Ushakov <just_lad@mail.ru>
2023-12-24 17:13:57 +0300

    task1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -3,26 +3,38 @@ import { Cell, toNano } from 'ton-core';
 import { Task1 } from '../wrappers/Task1';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import { exitCode } from 'process';
 
 describe('Task1', () => {
     let code: Cell;
+    let blockchain: Blockchain;
+
 
     beforeAll(async () => {
         code = await compile('Task1');
     });
 
-    let blockchain: Blockchain;
-    let task1: SandboxContract<Task1>;
-
     beforeEach(async () => {
+        // blockchain = await Blockchain.create();
+        // const deployer = await blockchain.treasury('deployer');
+
+        // task1 = blockchain.openContract(Task1.createFromConfig({}, code));
+        // const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('5.0'));
+    });
+
+    it('should deploy', async () => {
         blockchain = await Blockchain.create();
-
-        task1 = blockchain.openContract(Task1.createFromConfig({}, code));
-
         const deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1703422663n,
+                receiver: deployer.address,
+                seqno: 1n,
+            }, code));
 
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('5.0'));
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: task1.address,
@@ -31,8 +43,149 @@ describe('Task1', () => {
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and task1 are ready to use
+    it('should throw 119', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1803425163n,
+                receiver: deployer.address,
+                seqno: 0n,
+            }, code));
+        
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+        const sendExternalResult = await task1.sendUpdate(0x9df10277, 0, 10n, 12, 54);
+        expect(sendExternalResult.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 119,
+        })
     });
+
+    it('should throw 120', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1803425163n,
+                receiver: deployer.address,
+                seqno: 0n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        const sendExternalResult = await task1.sendUpdate(0x9df10277, 0, 10n, 1, 53);
+        expect(sendExternalResult.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 120,
+        })
+    });
+
+    it('should throw 121', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1803425163n,
+                receiver: deployer.address,
+                seqno: 0n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        const sendExternalResult = await task1.sendUpdate(0x9df10277, 0, -10n, 1, 54);
+        expect(sendExternalResult.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 121,
+        });
+    });
+
+    it('should throw 122', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1603425163n,
+                receiver: deployer.address,
+                seqno: 0n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        const result = await task1.sendUpdate(0x9df10277, 0, 1n, 1, 54);
+        console.log("Get execution time gas used:", result)
+        expect(result.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 122,
+        });
+    });
+
+    it('should update seqno and execution_time', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1803424303n, 
+                receiver: deployer.address,
+                seqno: 100n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        let locked_for = 10000n;
+
+        const sendExternalResult = await task1.sendUpdate(0x9df10277, 0, locked_for, 101, 54);
+        
+        console.log('Now is: ', await task1.getNow());
+        console.log('executionTime is: ', await task1.getExecutionTime());
+        console.log('seqno is: ', await task1.getSeqno());
+
+        expect(await task1.getSeqno()).toEqual(101n);
+        expect(await task1.getExecutionTime()).toEqual(2703424303n + locked_for);
+    });
+
+    it('should throw 124', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 1803425163n, 
+                receiver: deployer.address,
+                seqno: 1n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+        
+        const sendExternalResult = await task1.sendClaim(0xbb4be234, 1);
+        expect(sendExternalResult.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 124,
+        })
+    });
+
+    it('should send the whole balance', async () => {
+        blockchain = await Blockchain.create();
+        const deployer = await blockchain.treasury('deployer');
+
+        let task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 200n,
+                executionTime: 23n, 
+                receiver: deployer.address,
+                seqno: 1n,
+            }, code));
+        const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        const sendExternalResult = await task1.sendClaim(0xbb4be234, 1);
+        expect(sendExternalResult.transactions).toHaveTransaction({
+            success: true,
+        })
+    });
+
 });
--- wrappers/Task1.ts
+++ wrappers/Task1.ts
@@ -1,9 +1,23 @@
+import { randomAddress } from '@ton-community/test-utils';
+import { generateKeyPair } from 'crypto';
+import * as fs from "fs";
+import { mnemonicNew, mnemonicToWalletKey, sign } from "ton-crypto";
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type Task1Config = {};
+export type Task1Config = {
+    publicKey: bigint,
+    executionTime: bigint,
+    receiver: Address,
+    seqno: bigint,
+};
 
 export function task1ConfigToCell(config: Task1Config): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeUint(config.publicKey, 256) // public_key:uint256
+        .storeUint(config.executionTime,32) // execution_time:uint32
+        .storeAddress(config.receiver) // receiver:MsgAddressInt
+        .storeUint(config.seqno, 32) // seqno:uint32
+        .endCell();
 }
 
 export class Task1 implements Contract {
@@ -14,7 +28,12 @@ export class Task1 implements Contract {
     }
 
     static createFromConfig(config: Task1Config, code: Cell, workchain = 0) {
-        const data = task1ConfigToCell(config);
+        const data = beginCell()
+                        .storeUint(config.publicKey, 256) // public_key:uint256
+                        .storeUint(config.executionTime,32) // execution_time:uint32
+                        .storeAddress(config.receiver) // receiver:MsgAddressInt
+                        .storeUint(config.seqno, 32) // seqno:uint32
+                        .endCell();
         const init = { code, data };
         return new Task1(contractAddress(workchain, init), init);
     }
@@ -25,5 +44,43 @@ export class Task1 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+    
+    async getNow(provider: ContractProvider) {
+        const result = (await provider.get('get_now', [])).stack;
+        return result.readBigNumber();
+    }
+
+    async getSeqno(provider: ContractProvider) {
+        const result = (await provider.get('get_seqno', [])).stack;
+        return result.readBigNumber();
+    }
+
+    async getExecutionTime(provider: ContractProvider) {
+        const result = (await provider.get('get_execution_time', [])).stack;
+        return result.readBigNumber();
+    }
+
+    // update#9df10277 query_id:uint64 signature:bits512 ^[ locked_for:uint32 new_seqno:uint32 ] = ExtInMsgBody
+    async sendUpdate(provider: ContractProvider, opcode: number, query_id: number, 
+        locked_for: bigint, new_seqno: number, signature: number) {
+        let ref = beginCell()
+                    .storeInt(locked_for, 32)
+                    .storeUint(new_seqno, 32)
+                    .endCell()
+        await provider.external(beginCell()
+                                    .storeUint(opcode, 32)
+                                    .storeUint(query_id, 64)
+                                    .storeUint(signature, 512)
+                                    .storeRef(ref)
+                                    .endCell());
+    }
+
+    // claim#bb4be234 query_id:uint64 = ExtInMsgBody
+    async sendClaim(provider: ContractProvider, opcode: number, query_id: number) {
+        await provider.external(beginCell()
+                                    .storeUint(opcode, 32)
+                                    .storeUint(query_id, 64)
+                                    .endCell());
     }
 }
