3ca32379844cd0ee03e7a460a3a9bfc5176f7152
Ushakov <just_lad@mail.ru>
2023-12-30 14:21:38 +0300

    task2

--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,9 +1,25 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Dictionary,
+    Sender,
+    SendMode
+} from 'ton-core';
 
-export type Task2Config = {};
+export type Task2Config = {
+    owner: Address,
+    dict: Dictionary<bigint, number>,
+};
 
 export function task2ConfigToCell(config: Task2Config): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeAddress(config.owner)
+        .storeDict(config.dict)
+        .endCell();
 }
 
 export class Task2 implements Contract {
@@ -25,5 +41,58 @@ export class Task2 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendAddUser(provider: ContractProvider, via: Sender, value: bigint, user: Address, share: number) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0x368ddef3, 32).storeUint(0,64).storeAddress(user).storeUint(share, 32).endCell(),
+        });
+    }
+
+    async sendRemoveUser(provider: ContractProvider, via: Sender, value: bigint, user: Address) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0x278205c8, 32).storeUint(0,64).storeAddress(user).endCell(),
+        });
+    }
+
+    async sendSplitTon(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0x068530b3, 32).storeUint(0,64).endCell(),
+        });
+    }
+
+    async sendTransferNotification(provider: ContractProvider, via: Sender, value: bigint, amount: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(0x701c09a6, 32).storeUint(0,64).storeCoins(amount).endCell(),
+        });
+    }
+
+    async getUsers(provider: ContractProvider) {
+
+        const result = await provider.get('get_users', [        ]);
+        return {
+            out: result.stack.readCell(),
+            gasUsed: result.gasUsed,
+        };
+    }
+
+    async getUserShare(provider: ContractProvider, user: Address) {
+        let userSlice = beginCell().storeAddress(user).endCell()
+
+        const result = await provider.get('get_user_share', [
+            {type: 'slice', cell: userSlice}
+        ]);
+        return {
+            out: result.stack.readNumber(),
+            gasUsed: result.gasUsed,
+        };
     }
 }
